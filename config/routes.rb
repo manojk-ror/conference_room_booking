5287483f625dc66067eccc39465c28d544bf49d8
Manoj Kumar <manojk.ror@gmail.com>
2016-12-27 02:16:42 +0530

    added devise on initail level

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  # devise url's
+  devise_for :users
   # Root URL
   root to: "home#index"
 
