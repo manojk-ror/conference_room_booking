c0ea610372fefe5fc6307459cbce4e00153c4aef
Manoj K <manojk5@chetu.com>
2016-12-30 20:25:36 +0530

    added datetimepicker with weekdays disabled

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap.min
+//= require moment-with-locales
+//= require bootstrap-datetimepicker
 //= require custom
 //= require_tree .
--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,20 @@
 $(document).on('click', '.alert > span', function(){
   $('.alert').hide();
 });
+
+$(function () {
+  $('#datetimepicker1').datetimepicker({
+    minDate: moment(),
+    daysOfWeekDisabled: [0, 6]
+  });
+  $('#datetimepicker2').datetimepicker({
+    minDate: moment(),
+    daysOfWeekDisabled: [0, 6]
+  });
+  $("#datetimepicker1").on("dp.change", function (e) {
+    $('#datetimepicker2').data("DateTimePicker").minDate(e.date);
+  });
+  // $("#datetimepicker2").on("dp.change", function (e) {
+  //   $('#datetimepicker1').data("DateTimePicker").maxDate(e.date);
+  // });
+});
